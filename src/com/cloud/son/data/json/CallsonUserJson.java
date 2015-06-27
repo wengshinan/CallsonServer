6f136183e4d9247a5177ff1f462e9d4218d9a9d8
wengshinan <wengshinan@163.com>
2015-06-27 21:51:49 +0800

    test on mac

--- src/com/cloud/son/data/json/CallsonUserJson.java
+++ src/com/cloud/son/data/json/CallsonUserJson.java
@@ -18,13 +18,36 @@ import com.cloud.son.entity.constant.UserConstant;
  */
 public class CallsonUserJson extends CallsonUser implements ICreator<JSONObject>, IParser<JSONObject> {
 
+	@Override
+	public JSONObject create() {
+		JSONObject user = new JSONObject();
+		user.put(UserConstant.USER_PARAM_USERID, uId);
+		user.put(UserConstant.USER_PARAM_USERTYPE, type);
+		//user.put(UserConstant.USER_PARAM_USERPROPERTY, uProp == null ? null : uProp.create());
+
+		return user;
+	}
+
+	@Override
+	public void parse(JSONObject data) {
+		this.uId = data.isNull(UserConstant.USER_PARAM_USERID) ?
+				null : data.getString(UserConstant.USER_PARAM_USERID);
+		this.type = data.isNull(UserConstant.USER_PARAM_USERTYPE) ?
+				null : UserType.valueOf(data.getString(UserConstant.USER_PARAM_USERTYPE));
+		this.uProp = data.isNull(UserConstant.USER_PARAM_USERPROPERTY) ?
+				null : new UserPropertyJson(data.getJSONObject(UserConstant.USER_PARAM_USERPROPERTY));
+	}
+
 	public class UserPropertyJson extends CallsonUser.UserProperty implements ICreator<JSONObject>,
 			IParser<JSONObject> {
-		public UserPropertyJson(){}
-		public UserPropertyJson(JSONObject obj){
+		public UserPropertyJson() {
+		}
+
+		public UserPropertyJson(JSONObject obj) {
 			this.parse(obj);
 		}
-		public UserPropertyJson(String str){
+
+		public UserPropertyJson(String str) {
 			this(new JSONObject(str));
 		}
 
@@ -42,41 +65,21 @@ public class CallsonUserJson extends CallsonUser implements ICreator<JSONObject>
 
 		@Override
 		public void parse(JSONObject data) {
-			cnName = data.isNull(UserConstant.USER_PARAM_CNNAME) ? 
+			cnName = data.isNull(UserConstant.USER_PARAM_CNNAME) ?
 					null : data.getString(UserConstant.USER_PARAM_CNNAME);
-			enName = data.isNull(UserConstant.USER_PARAM_ENNAME) ? 
+			enName = data.isNull(UserConstant.USER_PARAM_ENNAME) ?
 					null : data.getString(UserConstant.USER_PARAM_ENNAME);
-			age = data.isNull(UserConstant.USER_PARAM_AGE) ? 
+			age = data.isNull(UserConstant.USER_PARAM_AGE) ?
 					0 : data.getInt(UserConstant.USER_PARAM_AGE);
-			description = data.isNull(UserConstant.USER_PARAM_DESCRIPTION) ? 
+			description = data.isNull(UserConstant.USER_PARAM_DESCRIPTION) ?
 					null : data.getString(UserConstant.USER_PARAM_DESCRIPTION);
-			phone = data.isNull(UserConstant.USER_PARAM_PHONE) ? 
+			phone = data.isNull(UserConstant.USER_PARAM_PHONE) ?
 					null : data.getString(UserConstant.USER_PARAM_PHONE);
-			password = data.isNull(UserConstant.USER_PARAM_PASSWORD) ? 
+			password = data.isNull(UserConstant.USER_PARAM_PASSWORD) ?
 					null : data.getString(UserConstant.USER_PARAM_PASSWORD);
 
 		}
 
-	}
-
-	@Override
-	public JSONObject create() {
-		JSONObject user = new JSONObject();
-		user.put(UserConstant.USER_PARAM_USERID, uId);
-		user.put(UserConstant.USER_PARAM_USERTYPE, type);
-		user.put(UserConstant.USER_PARAM_USERPROPERTY, uProp == null ? null : uProp.create());
-
-		return user;
-	}
-
-	@Override
-	public void parse(JSONObject data) {
-		this.uId = data.isNull(UserConstant.USER_PARAM_USERID) ? 
-				null : data.getString(UserConstant.USER_PARAM_USERID);
-		this.type = data.isNull(UserConstant.USER_PARAM_USERTYPE) ? 
-				null : UserType.valueOf(data.getString(UserConstant.USER_PARAM_USERTYPE));
-		this.uProp = data.isNull(UserConstant.USER_PARAM_USERPROPERTY) ? 
-				null : new UserPropertyJson(data.getJSONObject(UserConstant.USER_PARAM_USERPROPERTY));
 	}
 
 }
